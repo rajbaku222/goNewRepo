1b92b7179b70f800fbe73e1ec90a1f408ea46608
rajbaku222 <raj.baku@bytestechnolab.com>
2023-10-26 15:54:09 +0530

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajbaku222/goNewRepo
+
+go 1.20
--- test.go
+++ test.go
@@ -1,13 +1,19 @@
 package gonewrepo
 
+import "fmt"
 
 
-func MyFunc(){
+
+func MyFunc() {
 	type Address struct {
-		Name string 
-		Street string
-		City string
-		State string
+		Name    string
+		Street  string
+		City    string
+		State   string
 		Pincode int
 	}
+	r := Address{Name: "raj", Street: "nrroad", City: "talala", State: "gujrat", Pincode: 362150}
+	fmt.Println(r)
+
+	
 }
